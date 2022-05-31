7413e72a1fb317fb0c9dd1da233fed52ff488aca
bkawin <93135650+bkawin@users.noreply.github.com>
2022-05-31 14:46:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,57 @@
-def hello_world(request):
-    """
-    Responds to any HTTP request.
-    Args:
-        request (flask.Request): HTTP request object.
-    Returns:
-        The response text or any set of values that can be turned into a
-        Response object using
-        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
-    """
-    request_json = request.get_json()
-    if request.args and 'message' in request.args:
-        return request.args.get('message')
-    elif request_json and 'message' in request_json:
-        return request_json['message']
-    else:
-        return f'Hello World!'
+import json
+import requests
+import re
+import google.auth
+import google.auth.transport.requests
+from flask import jsonify
+
+creds, project = google.auth.default()
+
+def refAccessToken(creds):    
+    auth_req = google.auth.transport.requests.Request()
+    creds.refresh(auth_req)
+    authToken = creds.token
+    return authToken
+
+def response_get_Data(authToken,url):
+    headers = {
+       "Accept": "application/json",
+       "Authorization": "Bearer "+authToken
+     }
+    response = requests.request(
+       "GET",
+       url,
+       headers=headers
+     ) 
+    response = response.text.replace("u'", "'")
+    return response
+
+def response_post_Data(authToken,url):
+    headers = {
+       "Accept": "application/json",
+       "Authorization": "Bearer "+authToken
+     }
+    response = requests.request(
+       "POST",
+       url,
+       headers=headers
+     ) 
+    response = response.text.replace("u'", "'")
+    return response  
+
+def get_organisations(request):
+    org_list = []
+    project_url="https://cloudresourcemanager.googleapis.com/v1beta1/organizations:search"
+    authToken = refAccessToken(creds)
+    resp = response_post_Data(authToken,project_url)
+    #org_list = json.loads(resp)
+    #print(org_list)
+    #return org_list
+    #string = resp.read().decode('utf-8')
+    json_obj = json.loads(resp)
+    response_json = json_obj.decode('utf-8')
+    return json.loads(response_json)
+    #return json_obj
+    print(json.loads(response_json))
+    return jsonify(json.loads(response_json))
+    
